231ecaf49ba43798c3a9b910cb92c8915d11b6f1
namangupta17-web <namangupta8a17@gmail.com>
2026-02-10 17:53:28 +0000

    datatypes

---
+++ 01/datatypes.js
@@ -0,0 +1,15 @@
+"use alert" 
+// this is use which says we are using latest js!! not the old version.
+
+// code readibility should also be there
+// number- 2 to power 53
+// bigint-stores larger numbers
+// string=""
+// boolen=true/false
+// null-standalone avlue {representation of a empty datatype}
+// undefined-the value isnt given.
+// symbol -for uniqueness 
+console.log(typeof undefined) 
+// undefined
+console.log(typeof null)
+// object!!!!!

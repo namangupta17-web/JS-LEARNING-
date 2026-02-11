fab91eb7014386fab5cf711877a7af4114c8a834
namangupta17-web <namangupta8a17@gmail.com>
2026-02-11 21:17:58 +0000

    conversion of datatypes

---
+++ 01/conversion.js
@@ -0,0 +1,15 @@
+let score=33
+console.log(typeof (score))
+let str=String(score)
+console.log(typeof (str))
+/*
+33--"33"
+"33abc"=NaN(not a number)==>NaN its type is number
+null=0
+undefiner=NaN
+*/
+let a="naman"
+let c=""
+let b=Boolean(a)
+let d=Boolean(c)
+console.table([b,d])

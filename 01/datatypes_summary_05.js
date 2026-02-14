c85a4b8d09ab62470bfa91aaf5e670b29077de55
namangupta17-web <namangupta8a17@gmail.com>
2026-02-14 12:37:59 +0000

    dataypes_summary

---
+++ 01/datatypes_summary_05.js
@@ -0,0 +1,14 @@
+// Primitive 
+
+// in this type of datatype the data is called by value the copy of the value is send 
+
+// 7types:String,Number,Boolean,Bigint,Null,Undefined,Symbol
+let num=["1","2","3"]
+let obj={
+    name:"naman",
+    roll_no:51
+}
+console.log(typeof (num))
+
+// Reference(Non-primitative)
+// Array,Objects,Functions{all are called objects when typeof}

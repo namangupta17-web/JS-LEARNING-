18df4b2cccb0ccb6f0a90093d791f2272982035c
namangupta17-web <namangupta8a17@gmail.com>
2026-02-14 13:46:00 +0000

    string

---
+++ 01/string_06.js
@@ -0,0 +1,18 @@
+let name= new String('namaste')
+// in this we can use more prototypesss
+let yoy="yoyo"
+console.log(`my name is ${name.toUpperCase()}`)
+console.log(name.charAt(2))
+// tells kunsi position m kunsa character h 
+console.log(name.indexOf('t'))
+// tells kunsi index pr kunsi value h 
+let newString= name.substring(1,4)
+console.log(newString)
+let another_string=name.slice(-4,-1)
+console.log(another_string)
+const url="https://namanokhero"
+console.log(url.replace("ok", "hey"))
+let hey="namaste-sbhi-ji"
+console.log(hey.split('-'))
+
+

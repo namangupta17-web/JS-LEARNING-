ecb6495d89533b81028c7e011e85fc92c45bce8b
namangupta17-web <namangupta8a17@gmail.com>
2026-02-14 08:02:27 +0000

    comparsion

---
+++ 01/comparison_04.js
@@ -0,0 +1,14 @@
+/*console.log(2==2)
+console.log(2>=2)
+and much more */
+
+console.log("2"==2)
+// true
+console.log(null==0)
+console.log(null>0)
+console.log(null>=0)
+// all this full of confusion bcz of inconsistency also the comparision operator and assignment operator has different syntaxs hence answer can differ with azib azib si values.
+
+//  ===(strict comparison as this also checks datatypes)
+console.log("2"===2)
+// false

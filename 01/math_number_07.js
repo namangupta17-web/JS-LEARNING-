c426899a2a2dbc9038ee69b1fdf6ef7dc86023cb
namangupta17-web <namangupta8a17@gmail.com>
2026-02-15 08:51:08 +0000

    msths

---
+++ 01/math_number_07.js
@@ -0,0 +1,27 @@
+
+let num=400.3932796393923
+let num_1= new Number(500)
+// console.log( num.toString.length)
+// console.log( num.toPrecision(6))
+// console.log( num.toFixed(5))
+// console.log( num_1)
+// console.log( num)
+// let num_2= 100000
+// console.log(num_2.toLocaleString('en-In'))
+
+// ++++++++++Maths++++++++
+// console.log(Math)
+// console.log(Math.round(4.5))
+// console.log(Math.floor(4.9))
+// console.log(Math.abs(-5))
+// console.log(Math.ceil(4.1))
+// console.log(Math.min(4,2,3,6,7))
+// console.log(Math.max(5,6,7,8,9))
+
+console.log(Math.random())
+console.log(Math.floor((Math.random()*10)+Math.random()*(10)))
+console.log(Math.floor(Math.random()*10))
+let max=20
+let min=10
+console.log(Math.floor((Math.random()*(max-min))+1))
+

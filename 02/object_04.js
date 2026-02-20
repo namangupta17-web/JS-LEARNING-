a6e1f0575da305e4f1ecb9c31c92bc14544999aa
namangupta17-web <namangupta8a17@gmail.com>
2026-02-20 15:30:46 +0000

    object_02

---
+++ 02/object_04.js
@@ -0,0 +1,34 @@
+const myarr= new Object(
+    name="sammy",
+    cllgId="123ab"
+)
+// this is consturctor method hence the above value is singelton
+console.log(typeof myarr)
+// do see this gives few errors so the correct method for this is mentioned below
+myarr.name="naman"
+myarr.cllgId="abcd"
+console.log(myarr)
+
+const ok={
+    name:{
+        fullname:{
+            firstname:"Naman",
+            lastname:"Gupta"
+        },
+        shortname:"Naman"
+    }
+}
+console.log(ok.name.fullname)
+
+// Different ways of concatining two objects
+const obj1={a:1,b:2}
+const obj2={c:1,d:2}
+const obj3={obj1,obj2}
+// the problem with this is that it will give index 0 to whole obj1 also index 1 to whole object 2
+const obj4=Object.assign({},obj1, obj2)
+// this is acting as target {} while obj1 and obj2 are acting as source
+const obj5={...obj1,...obj2}
+// the best method
+console.log(obj3)
+console.log(obj4)
+console.log(obj5)
